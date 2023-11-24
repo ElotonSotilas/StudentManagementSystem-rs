dac1e04eccd9044518e0edd7021d9ea16ec413a9
George Vassilev <george.vassilev91@gmail.com>
2023-11-24 13:28:47 +0200

    Changed table creation query

--- src/backend/sqlite_conn.rs
+++ src/backend/sqlite_conn.rs
@@ -24,29 +24,21 @@ impl DatabaseConnection {
                 r#"
             BEGIN;
             CREATE TABLE "USERS" (
-                "id" INTEGER PRIMARY KEY NOT NULL,
+                "id" INTEGER NOT NULL UNIQUE,
                 "username" TEXT NOT NULL,
                 "password" TEXT NOT NULL,
-                "email" TEXT NOT NULL,
+                "email" TEXT NOT NULL UNIQUE,
                 "phone" TEXT NOT NULL,
-                "created_at" DATE NOT NULL DEFAULT DATE('now'),
-                "updated_at" DATE NOT NULL DEFAULT DATE('now'),
                 "verified" BOOLEAN NOT NULL,
                 "suspended" BOOLEAN NOT NULL,
                 "forcenewpw" BOOLEAN NOT NULL,
-                "role" TEXT NOT NULL
+                "role" TEXT NOT NULL,
+                PRIMARY KEY("id" AUTOINCREMENT)
             );
-            
-            CREATE TRIGGER "update_time_USERS"
-            AFTER UPDATE ON "USERS"
-            FOR EACH ROW
-            BEGIN
-               UPDATE "USERS" SET "updated_at" = DATE('now') WHERE "id" = OLD."id";
-            END;
-            
+
             CREATE TABLE "STUDENT_ACCOUNT" (
-                "id" INTEGER PRIMARY KEY NOT NULL,
-                "student_id" INTEGER NOT NULL,
+                "id" INTEGER NOT NULL UNIQUE,
+                "student_id" INTEGER NOT NULL UNIQUE,
                 "advisor_id" INTEGER NOT NULL,
                 "discipline" TEXT NOT NULL,
                 "enrollment" TEXT NOT NULL,
@@ -54,35 +46,29 @@ impl DatabaseConnection {
                 "can_grad" BOOLEAN NOT NULL CHECK ("cgpa" > 2.0 AND "cum_credit" >= 120),
                 "cur_credit" INTEGER NOT NULL,
                 "cum_credit" INTEGER NOT NULL,
-                FOREIGN KEY ("student_id") REFERENCES USERS("id"),
-                FOREIGN KEY ("advisor_id") REFERENCES USERS("id")
+                FOREIGN KEY ("student_id") REFERENCES "USERS"("id"),
+                FOREIGN KEY ("advisor_id") REFERENCES "USERS"("id"),
+                PRIMARY KEY("id" AUTOINCREMENT)
             );
             
             CREATE TABLE "TEACHER_ACCOUNT" (
-                "id" INTEGER PRIMARY KEY NOT NULL,
+                "id" INTEGER NOT NULL,
                 "teacher_id" INTEGER NOT NULL,
                 "dept_id" INTEGER NOT NULL,
                 "dept" TEXT NOT NULL,
-                FOREIGN KEY ("teacher_id") REFERENCES USERS("id"),
-                FOREIGN KEY ("dept_id") REFERENCES DEPARTMENTS("id")
+                FOREIGN KEY ("teacher_id") REFERENCES "USERS"("id"),
+                FOREIGN KEY ("dept_id") REFERENCES "DEPARTMENTS"("id"),
+                PRIMARY KEY("id" AUTOINCREMENT)
             );
             
             CREATE TABLE "COURSES" (
-                "id" INTEGER PRIMARY KEY NOT NULL,
+                "id" INTEGER NOT NULL,
                 "teacher_id" INTEGER NOT NULL,
                 "course" TEXT NOT NULL,
                 "cr_cost" INTEGER NOT NULL,
-                "created_at" DATE NOT NULL DEFAULT DATE('now'),
-                "updated_at" DATE NOT NULL DEFAULT DATE('now'),
-                FOREIGN KEY ("teacher_id") REFERENCES USERS("id")
+                FOREIGN KEY ("teacher_id") REFERENCES USERS("id"),
+                PRIMARY KEY("id" AUTOINCREMENT)
             );
-            
-            CREATE TRIGGER "update_time_COURSES"
-            AFTER UPDATE ON "COURSES"
-            FOR EACH ROW
-            BEGIN
-               UPDATE "COURSES" SET "updated_at" = DATE('now') WHERE "id" = OLD."id";
-            END;
             
             CREATE TABLE "STUDENT_COURSES" (
                 "student_id" INTEGER NOT NULL,
@@ -94,10 +80,11 @@ impl DatabaseConnection {
             );
             
             CREATE TABLE "DEPARTMENTS" (
-                "id" INTEGER PRIMARY KEY NOT NULL,
+                "id" INTEGER NOT NULL,
                 "dept_head" INTEGER NOT NULL,
                 "name" TEXT NOT NULL,
-                FOREIGN KEY ("dept_head") REFERENCES TEACHER_ACCOUNT(id)
+                FOREIGN KEY ("dept_head") REFERENCES TEACHER_ACCOUNT("id"),
+                PRIMARY KEY("id" AUTOINCREMENT)
             );
             COMMIT;
             "#,
